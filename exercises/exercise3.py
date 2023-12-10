9f349e8ff632a889c90a92d14802ef737f0d6826
Ashwin <aprabhu1994@gmail.com>
2023-12-10 17:18:25 +0100

    Exercise 3

---
+++ exercises/exercise3.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import sqlite3
+
+# Step 1: Download the data
+url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
+df = pd.read_csv(url, sep=";", encoding='ISO-8859-1', skiprows=6, skipfooter=4, engine='python')
+
+
+# Step 2: Reshape the data structure
+columns_to_keep = ['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2', 'Insgesamt', 'Insgesamt.1', 'Insgesamt.2', 'Insgesamt.3', 'Insgesamt.4', 'Insgesamt.5', 'Insgesamt.6']
+new_column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
+
+column_mapping = dict(zip(columns_to_keep, new_column_names))
+
+df.rename(columns=column_mapping, inplace=True)
+
+df = df[new_column_names]
+
+# Step 3: Validate data
+# Validate CINs
+df['CIN'] = df['CIN'].astype(str).str.zfill(5)
+
+# Validate positive integers
+df = df.apply(pd.to_numeric, errors='coerce')
+df = df.dropna()
+
+# Validate positive integers
+df = df[(df['date'].astype(str).str.isnumeric()) &
+        (df['name'].astype(str).str.isnumeric()) &
+        (df['CIN'].astype(str).str.isnumeric()) &
+        (df[['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']] > 0).all(axis=1)]
+
+# Step 4: Use fitting SQLite types
+# Define SQLite types
+sqlite_types = {'date': 'TEXT', 'CIN': 'TEXT', 'name': 'TEXT',
+                'petrol': 'INTEGER', 'diesel': 'INTEGER', 'gas': 'INTEGER',
+                'electro': 'INTEGER', 'hybrid': 'INTEGER', 'plugInHybrid': 'INTEGER', 'others': 'INTEGER'}
+
+# Step 5: Write data to SQLite database
+conn = sqlite3.connect('cars.sqlite')
+df.to_sql('cars', conn, index=False, if_exists='replace', dtype=sqlite_types)
+conn.close()
